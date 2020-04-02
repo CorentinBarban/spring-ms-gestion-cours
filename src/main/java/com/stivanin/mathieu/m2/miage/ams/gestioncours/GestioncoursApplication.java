6fb0ee6cadd64e9df7eab9a8076e76627a2ec2cb
Mathieu <math.stiv@gmail.com>
2020-04-02 10:19:56 +0200

    Création projet

---
+++ src/main/java/com/stivanin/mathieu/m2/miage/ams/gestioncours/GestioncoursApplication.java
@@ -0,0 +1,13 @@
+package com.stivanin.mathieu.m2.miage.ams.gestioncours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioncoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioncoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stivanin/mathieu/m2/miage/ams/gestioncours/GestioncoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stivanin.mathieu.m2.miage.ams.gestioncours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
