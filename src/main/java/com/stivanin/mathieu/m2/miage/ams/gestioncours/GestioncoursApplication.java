0a667cdc104c825029cd7ae08efd5e436770957c
cbarban <corentin.barban@sfr.fr>
2020-06-08 11:45:45 +0200

    Documentation et mise a jour automatique des status

--- src/main/java/com/stivanin/mathieu/m2/miage/ams/gestioncours/GestioncoursApplication.java
+++ src/main/java/com/stivanin/mathieu/m2/miage/ams/gestioncours/GestioncoursApplication.java
@@ -1,11 +1,21 @@
 package com.stivanin.mathieu.m2.miage.ams.gestioncours;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Gestion cours", version = "v1"))
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
 public class GestioncoursApplication {
 
     public static void main(String[] args) {
